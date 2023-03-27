10114347e26e375e0370b8de40edb0eedd100a71
JAFAR564 <129004755+JAFAR564@users.noreply.github.com>
2023-03-27 05:50:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function getResult(value) {
+    document.getElementById("result").value += value;
+  }
+  
+  function clearResult() {
+    document.getElementById("result").value = "";
+  }
+  
+  function calculate() {
+    var result = eval(document.getElementById("result").value);
+    document.getElementById("result").value = result;
+  }
+  function getResult(value) {
+    document.getElementById("result").value += value;
+  }
+  
+  function clearResult() {
+    document.getElementById("result").value = "";
+  }
+  
+  function calculate() {
+    var result = eval(document.getElementById("result").value);
+    document.getElementById("result").value = result;
+  }
+    
